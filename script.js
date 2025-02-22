e469a30b7a50a5596307c4f0e6b321cdb79c37ad
Vishal Singh <96664216+quantum0X@users.noreply.github.com>
2025-02-22 15:16:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+	const button = document.getElementById('button');
+	const colors = document.getElementById('colorSelect');
+  
+  button.addEventListener('click', function () {
+		selectedColor = colors.options[colors.selectedIndex]
+    colors.removeChild(selectedColor)    
+  })
+})
